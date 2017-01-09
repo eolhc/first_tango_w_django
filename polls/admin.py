5f28d5735c6e108513db15b059464cacffaa6455
Chloe Phua <cqphua@gmail.com>
2017-01-09 21:23:41 +1100

    add database and tables

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+
+
 # Register your models here.
+#Just one thing to do: we need to tell the admin that
+# Question objects have an admin interface
+
+from .models import Question
+
+admin.site.register(Question)
